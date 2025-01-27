72f078548ca63f3c27c7a5835f6482136d391236
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-27 17:12:18 +0530

    we write code for converting binary to decimal

---
+++ intro/binToDec.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class binToDec {
+    public static int binToDecimal(int binary){
+        // Your code here
+        int dec = 0;
+        int i = 0 ;
+        while(binary > 0){
+            int firstBitFromBin = binary % 10;
+            dec += (int)(firstBitFromBin * Math.pow(2,i));
+            binary = binary/10;
+            i++;
+        }
+        return dec;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your binary number : ");
+        int binaryNumber = sc.nextInt();
+        System.out.println(binaryNumber + " ---> " + binToDecimal(binaryNumber));
+    }
+}
