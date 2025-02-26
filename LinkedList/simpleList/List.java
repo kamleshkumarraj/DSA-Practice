fb4e743e34eeae42a2d0c478f25115836f700780
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-02-26 21:31:02 +0530

    We create n node for simple linkedList

---
+++ LinkedList/simpleList/List.java
@@ -0,0 +1,14 @@
+package LinkedList.simpleList;
+
+public class List {
+    Node head;
+    Node tail;
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+    }
+}
