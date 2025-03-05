cd0e27f402fa5b595c3057ef7f3d2cf665f195cb
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-03-06 04:27:41 +0530

    We write method for pop the element from list

--- LinkedList/simpleList/List.java
+++ LinkedList/simpleList/List.java
@@ -30,7 +30,12 @@ public class List {
         this.size++;
     }
 
+    // method for insert value in list at any index.
     public void insertAtIndex(int data, int idx){
+        if(idx > this.count()) {
+            System.out.println("Please provide valid index !");
+            return;
+        }
         if(idx==0) {
             this.insertAtBeginning(data);
             return;
@@ -46,7 +51,32 @@ public class List {
         }
         temp.next = temp1.next;
         temp1.next = temp;
+        this.size ++ ;
     }
+
+    // method for delete last element from list.
+    public void pop(){
+        if(this.count() == 0){
+            System.out.println("List is already empty !");
+            return;
+        }
+
+        if(this.count() == 1){
+            head = null;
+            tail = null;
+            this.size -= this.size;
+            return;
+
+        }
+        Node temp = head;
+        while(temp.next.next !=null) temp = temp.next;
+
+        temp.next = null;
+        tail = temp;
+        this.size -= this.size;
+
+    }
+
 
     public int count(){
         return size;
@@ -57,17 +87,22 @@ public class List {
 
         List list = new List();
         list.insertNode(20);
-        list.insertNode(30);
-        list.insertNode(40);
-        list.insertNode(50);
-        list.insertNode(60);
+//        list.insertNode(30);
+//        list.insertNode(40);
+//        list.insertNode(50);
+//        list.insertNode(60);
+//
+//        list.insertAtBeginning(10);
+//        list.insertAtBeginning(0);
+        list.printList();
+//
+//        list.insertAtIndex(80,7);
+//        list.printList();
 
-        list.insertAtBeginning(10);
-        list.insertAtBeginning(0);
-
+        list.pop();
         list.printList();
 
-        list.insertAtIndex(80,7);
+        list.pop();
         list.printList();
 
 //        list.insertAtIndex(70,4);
